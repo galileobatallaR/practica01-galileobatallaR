569e31d095e9d5ceac532aa30e23f17dffe00e1a
galileobatallaR <galileo1324@hotmail.com>
2026-02-27 20:40:23 -0600

    Update ej2.c

--- soluciones/ej2.c
+++ soluciones/ej2.c
@@ -1,6 +1,11 @@
-
 #include <stdio.h>
-int main(){
-    // Resolver ejercicio 2
+
+int main() {
+    int m[2][4];
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 4; j++) {
+            printf("%p\n", (void*)&m[i][j]);
+        }
+    }
     return 0;
 }
